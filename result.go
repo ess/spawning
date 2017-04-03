54aed24bcac0be2e4783302de7865c3db6157f88
Dennis Walters <dwalters@engineyard.com>
2017-04-03 19:50:08 -0400

    Improved description of Result

--- result.go
+++ result.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 )
 
-// Result represents the actual result of a spawned command
+// Result represents the actual result of a spawned command. The Command member
+// contains the actual command that was run. The Output member is the combined
+// STDOUT and STDERR of the command. The Success member reflects whether or not
+// the command had a successful exit status.
 type Result struct {
 	Command string
 	Output  string
